03b37042cd841285b1505f3455f878511fa45e1e
Lucefull <luansteffens@gmail.com>
2022-08-22 18:09:46 -0300

    Feat: Created navigation bar and default page

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import PaginaPadrao from './components/PaginaPadrao';
 import Inicio from './pages/Inicio';
 import Menu from './pages/Menu';
 
 export default function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Inicio/>} />
-        <Route path='/cardapio' element={<Menu/>}/>
-      </Routes>
-    </Router>
+    <main>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Menu />} />
+          </Route>
+        </Routes>
+      </Router>
+    </main>
   );
 }
